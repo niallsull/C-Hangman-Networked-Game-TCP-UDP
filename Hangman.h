ae5946f50db631fe5283ec8047daa5020531fc4f
niallsull <niallsull@yahoo.co.uk>
2014-04-30 12:59:09 +0100

    Create Hangman.h

---
+++ Hangman.h
@@ -0,0 +1,17 @@
+#ifndef HANGMAN_H
+#define HANGMAN_H
+
+#include <syslog.h>
+ #include <errno.h>
+
+
+ 
+ # define NUM_OF_WORDS (sizeof (word) / sizeof (word [0]))
+ # define MAXLEN 80 /* Maximum size in the world of Any string */
+ 
+
+#include <stdio.h>
+#include <string.h>
+play_hangman (int in, int out);
+
+#endif 
